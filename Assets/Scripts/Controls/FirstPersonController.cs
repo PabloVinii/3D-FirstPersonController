15f6e2b6fefba6a1f926d4c85975121a148f66d0
PabloVinii <pvinicius006@hotmail.com>
2023-05-09 23:52:13 -0300

    player jump

--- Assets/Scripts/Controls/FirstPersonController.cs
+++ Assets/Scripts/Controls/FirstPersonController.cs
@@ -7,23 +7,29 @@ public class FirstPersonController : MonoBehaviour
     // Start is called before the first frame update
     public bool canMove { get; private set;} = true;
     private bool isSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool shouldJump => Input.GetKeyDown(jumKey) && characterController.isGrounded;
 
     [Header("Functional Options")]
     [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
 
     [Header("Controls")]
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumKey = KeyCode.Space;
 
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 5.0f;
     [SerializeField] private float sprintSpeed = 8.0f;
-    [SerializeField] private float gravity = 30.0f;
 
     [Header("Look Parameters")]
     [SerializeField, Range(1,10)] private float lockSpeedX = 2.0f;
     [SerializeField, Range(1,10)] private float lockSpeedY = 2.0f;
     [SerializeField, Range(1,180)] private float upperLockLimit = 80.0f;
     [SerializeField, Range(1,180)] private float lowerLockLimit = 80.0f;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -48,16 +54,29 @@ public class FirstPersonController : MonoBehaviour
             HandleMovementInput();
             HandleMouseLook();
 
+            if (canJump )
+            {
+                HandleJump();
+            }
             ApplyFinalMovements();
         }
     }
     private void HandleMovementInput() {
         currentInput = new Vector2((isSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (isSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
         float moveDirectionY = moveDirection.y;
+        moveDirection = moveDirection.normalized * Mathf.Clamp(moveDirection.magnitude, 0, walkSpeed);
         moveDirection = (transform.TransformDirection(Vector3.forward * currentInput.x) + transform.TransformDirection(Vector3.right * currentInput.y));
         moveDirection.y = moveDirectionY;
     }
 
+    private void HandleJump()
+    {
+        if (shouldJump)
+        {   
+            moveDirection.y = jumpForce;
+        }
+    }
+    
     private void HandleMouseLook() {
         rotationX -= Input.GetAxis("Mouse Y") * lockSpeedY;
         rotationX = Mathf.Clamp(rotationX, -upperLockLimit,lowerLockLimit);
